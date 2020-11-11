90f45659e9eb1842d54f536f7be6f314077d73cd
Rodrigo Denúbila <rdenubila@gmail.com>
2020-11-11 14:07:21 -0300

    informações dos sócios nos detalhes dos licitantes

--- src/api/services/licitante.js
+++ src/api/services/licitante.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
     const getLicitanteById = (id) => {
 
         var lics = licitante_detalhe.filter(v=>v.num_documento==id);
+        var socios = [];
 
         lics.forEach(l=>{
             geral = licitacoes.find(v=>v.seq_dim_licitacao ==l.seq_dim_licitacao );
@@ -17,8 +18,21 @@ module.exports = (app) => {
 
         });
 
+        if(lics.length>0) {
+            var nomes = lics[0].nome_socio.split(";");
+            var cpf = lics[0].cpf_cnpj_socio.split(";");
+
+            for(i=0; i<nomes.length; i++){
+                socios.push({
+                    nome: nomes[i].trim(),
+                    cpf_cnpj: cpf[i].trim()
+                });
+            }
+        }
+
         return {
             infos: licitantes.find(v=>v.num_documento==id),
+            socios: socios,
             licitacoes: lics
         };
     };
